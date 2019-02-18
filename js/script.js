27c28ba908fb454d622133f38358b6a1bc5fe4c3
Remon <remon.dev@icloud.com>
2019-02-18 20:57:47 +0100

    commented

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
+/********
+Remon Tewodros
+*****/
 
-
-
-
-//page
-const pageClass = document.getElementsByClassName('page');
-
+// .page
+const pageClass = document.querySelector('.page');
 //search students
 const pageHeader = document.getElementsByClassName('page-header');
 const studentSearch = document.getElementsByClassName('student-search');
@@ -20,20 +19,8 @@ const studentDetails = document.getElementsByClassName('student-details');
 const avatar = document.getElementsByClassName('avatar');
 const email = document.getElementsByClassName('email');
 
-let currentPage = 1;
 let itemsPerPage = 10;
-let pageList = new Array();
 let numberOfPages = 1; // calculates the total number of pages
-
-function load() {
-   studentList;
-   numberOfPages = getNumberOfPages();
-}
-
-function getNumberOfPages() {
-   return Math.ceil(studentList.length / itemsPerPage);
-}
-
 
 // show only 10 students per page, and 'display none' the rest.
 const showPage = (studentList, page) => {
@@ -41,6 +28,7 @@ const showPage = (studentList, page) => {
       let firstItem = ((page * itemsPerPage) - 10);
       let lastItem = ((firstItem + itemsPerPage) - 1);
       let students = studentList[i];
+
       if (i >= firstItem && i <= lastItem) {
          students.style.display = 'block';
       } else {
@@ -49,62 +37,41 @@ const showPage = (studentList, page) => {
    }
 }
 
-
-
+//creates all the pagination buttons, adds them to the DOM, and adds their functionality.
 const appendPageLinks = (list) => {
-   numberOfPages;
-   let div = document.createElement('div');
-   let ul = document.createElement('ul');
 
-   div.className = 'pagination';
-   pageClass.appendChild(div);
-   div.appendChild(ul);
+   //clalculate how many pages are needed for the list
+   numberOfPages = Math.ceil(studentList.length / itemsPerPage);
 
+   const div = document.createElement('div'); //Create a div, give it the “pagination” class
+   const ul = document.createElement('ul'); //Create ul element
 
-   for (let i = 0; i < numberOfPages.length; i++) {
+   div.className = 'pagination'; // give the div the “pagination” class
+   pageClass.appendChild(div); // append the div to .page 
+   div.appendChild(ul); //Add a ul to the “pagination” div to store the pagination links
+
+   //add li and a tags with the page number text for every page
+   for (let i = 0; i < numberOfPages; i++) {
       const li = document.createElement('li');
+      ul.appendChild(li);
       const a = document.createElement('a');
-      a.textContent = i + 1;
-      nuberOfPages[i] = ul.appendChild(a);
+      a.textContent = 1 + i;
+      a.href = '#';
+      li.appendChild(a);
 
+      // Loop over pagination links to remove active class from all links
       a.addEventListener('click', (e) => {
-         e.preventDefault();
-         for (let i = 0; i < a.length; i++) {
-            const links = links[i];
-            a.classList.remove('active');
-         }
-         const aLink = document.getElementsByTagName('a');
-         for (let i = 0; i < array.length; i++) {
-
-            if (aLink[i] = e.target) {
-               e.target.className = 'active';
-            } else {
-               aLink.classList.remove = 'active';
-            }
-         }
-
+         e.preventDefault()
+         const links = document.getElementsByTagName('a');
          showPage(studentList, i + 1);
+         for (let i = 0; i < links.length; i++) {
+            links[i].classList.remove('active');
+         }
+         e.target.className = 'active';
       });
    }
-
 }
 
-function loadList() {
-   let begin = ((currentPage - 1) * itemsPerPage);
-   let end = begin + itemsPerPage;
-
-   pageList = studentList.slice(begin, end);
-   drawList();
-   check();
-}
-
-function drawList() {
-   pageClass.innerHTML = "";
-
-   for (i = 0; i < pageList.length; i++) {
-      pageClass.innerHTML += pageList[i] + "";
-   }
-}
-
-window.onload = showPage(studentList, 1);
-window.onload = appendPageLinks(studentList);
+// call the functions
+showPage(studentList, 1);
+appendPageLinks(studentList);
